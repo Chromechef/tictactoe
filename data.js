5559c8ad1eff6aef1922d27699a3759b6f10376d
Jimmy <70441896+Chromechef@users.noreply.github.com>
2023-02-08 16:42:45 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,38 @@
+const animals = [
+    {
+        "id": 1,
+        "img": "david-clode-7_TTPznVIQI-unsplash.jpg"
+    },
+    {
+        "id": 2,
+        "img": "karina-vorozheeva-rW-I87aPY5Y-unsplash.jpg"
+    },
+    {
+        "id": 3,
+        "img": "karsten-winegeart-NE0XGVKTmcA-unsplash.jpg"
+    },
+    {
+        "id": 4,
+        "img": "karsten-winegeart-oU6KZTXhuvk-unsplash.jpg"
+    },
+    {
+        "id": 5,
+        "img": "kevin-mueller-KBHqwZ4z69Y-unsplash.jpg"
+    },
+    {
+        "id": 6,
+        "img": "luisa-peter-7Xp9wDCxivc-unsplash.jpg"
+    },
+    {
+        "id": 7,
+        "img": "magdalena-smolnicka-MySy-2bd930-unsplash.jpg"
+    },
+    {
+        "id": 8,
+        "img": "raoul-droog-yMSecCHsIBc-unsplash.jpg"
+    }
+]
+
+
+
+export { animals }
